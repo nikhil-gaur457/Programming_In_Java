1e445f45f5dd9fcaaa8c8330f0df456e1ed282ff
Nikhil Gaur <nikhil.gaur_cs19@gla.ac.in>
2020-11-03 17:36:47 +0530

    make a hello program

---
+++ src/selfPractice/HelloWorld.java
@@ -0,0 +1,14 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Nikhil Gaur (n-g457)
+ *   Date: 02-11-2020
+ *   Time: 17:29
+ *   File: HelloWorld.java
+ */
+
+package selfPractice;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
