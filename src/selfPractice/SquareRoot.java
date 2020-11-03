d76b98740dd8bb6f7d0b658dede3b0e543a56a30
Nikhil Gaur <nikhil.gaur_cs19@gla.ac.in>
2020-11-03 17:50:51 +0530

    create a program to find square root of any number

---
+++ src/selfPractice/SquareRoot.java
@@ -0,0 +1,21 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Nikhil Gaur (n-g457)
+ *   Date: 03-11-2020
+ *   Time: 17:37
+ *   File: SquareRoot.java
+ */
+
+package selfPractice;
+
+import java.util.Scanner;
+
+public class SquareRoot {
+    public static void main(String[] args) {
+        double number,result;
+        System.out.print("Please enter a number to find square root : ");
+        Scanner scanner = new Scanner(System.in);
+        number = scanner.nextDouble();
+        result = number*number;
+        System.out.println("Square root of given number is " + result);
+    }
+}
