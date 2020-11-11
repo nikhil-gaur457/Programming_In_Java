c129aaec4a1050e3fbc6d851516faa6a15587239
Nikhil Gaur <nikhil.gaur_cs19@gla.ac.in>
2020-11-11 19:00:36 +0530

    create a program to find the LCM of two numbers.

---
+++ src/selfPractice/Lcm.java
@@ -0,0 +1,28 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Nikhil Gaur (n-g457)
+ *   Date: 11-11-2020
+ *   Time: 18:51
+ *   File: Lcm.java
+ */
+
+package selfPractice;
+
+import java.util.Scanner;
+
+public class Lcm {
+    public static void main(String[] args) {
+        int gcd = 0, LCM;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter both numbers : ");
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        scanner.close();
+        for (int i = 1; i <= num1 && i <= num2; i++) {
+            if (num1 % i == 0 && num2 % i == 0) {
+                gcd = i;
+            }
+        }
+        LCM = (num1*num2)/gcd;
+        System.out.println("L.C.M of both numbers is " + LCM);
+    }
+}
