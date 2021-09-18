8730ee8d3e44ac1c9e6e20139c9cdb37a03ae576
nikhil-gaur457 <nikhil.gaur_cs19@gla.ac.in>
2021-09-18 10:20:48 +0530

    Create a java class to demonstrate how HashSet  in java works.

---
+++ src/collection/framework/set/TreeSetExample.java
@@ -0,0 +1,34 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Nikhil gaur (nikhil-gaur457)
+ *   Date: 17-09-2021
+ *   Time: 19:10
+ *   File: TreeSetExample.java
+ */
+
+package collection.framework.set;
+
+import java.util.TreeSet;
+
+public class TreeSetExample {
+    // The TreeSet class uses a Tree for storage. The ordering of the elements is maintained by a set using their natural ordering whether or not an explicit comparator is provided.
+    public static void main(String[] args) {
+        TreeSet<String> treeSet = new TreeSet<>();
+        treeSet.add("Nikhil");
+        treeSet.add("Gaur");
+        treeSet.add("B.tech");
+        treeSet.add("Computer Science");
+        treeSet.add("Third Semester");
+        treeSet.add("D Show");
+
+
+        /*
+        TreeSet<Integer> treeSet = new TreeSet<>();
+        treeSet.add(1);
+        treeSet.add(2);
+        treeSet.add(3);
+        treeSet.add(4);
+        treeSet.add(5);
+        */
+        for (String s : treeSet) System.out.println(s);
+    }
+}
