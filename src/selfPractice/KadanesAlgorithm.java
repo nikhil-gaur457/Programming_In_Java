0e8027b8d3e83a0abbaa0e12a1e6111617015d88
nikhil-gaur457 <nikhil.gaur_cs19@gla.ac.in>
2021-10-10 15:44:36 +0530

    Kadane's Algorithm

--- src/selfPractice/KadanesAlgorithm.java
+++ src/selfPractice/KadanesAlgorithm.java
@@ -7,25 +7,30 @@
 
 package selfPractice;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 class KadaneAlgorithm {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the size of array : ");
         int sizeOfArray = scanner.nextInt();
         int[] arr = new int[sizeOfArray];
+        System.out.println("Enter the array elements");
         for (int i = 0; i < sizeOfArray; i++) {
             arr[i] = scanner.nextInt();
         }
-        Arrays.sort(arr);
-        int subArraySum = 0;
-        for (int i = 0; i < sizeOfArray; i++) {
-            subArraySum = subArraySum + arr[i];
+        int ans = maxSumOfSubArray(arr, sizeOfArray);
+        System.out.println("Maximum contiguous sum is : " + ans);
+    }
+
+    public static int maxSumOfSubArray(int[] a, int size) {
+        int max = a[0];
+        int currentMax = a[0];
+        for (int i = 1; i < size; i++) {
+            currentMax = Math.max(a[i], currentMax + a[i]);
+            max = Math.max(max, currentMax);
         }
-        if (subArraySum < 0) {
-            subArraySum = arr[sizeOfArray-1];
-        }
-        System.out.println(subArraySum);
+        return max;
     }
 }
+
