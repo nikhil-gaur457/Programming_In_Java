f9f7bd6e4e75b1f3f4e673caab91797818c45aff
nikhil-gaur457 <nikhil.gaur_cs19@gla.ac.in>
2021-10-10 15:58:05 +0530

    practice question

---
+++ src/selfPractice/Practice1.java
@@ -0,0 +1,19 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Nikhil gaur (nikhil-gaur457)
+ *   Date: 10-10-2021
+ *   Time: 15:46
+ *   File: Practice1.java
+ */
+
+package selfPractice;
+
+public class Practice1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 3; j++) {
+                System.out.print((char) (i + 96));
+            }
+            System.out.println("/n");
+        }
+    }
+}
